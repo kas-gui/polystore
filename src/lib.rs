59935a0e823eb81d8e64cef98c7f21b6fb715f27
Diggory Hardy <git@dhardy.name>
2021-07-07 20:49:45 +0100

    Add Entry API and remove method

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,17 @@
 
 // TODO: no_std?
 
-use std::any::TypeId;
+use std::any::{type_name, TypeId};
+use std::collections::hash_map as std_hm;
 use std::collections::HashMap;
 use std::hash::Hash;
 use std::marker::PhantomData;
 use std::mem::size_of;
 use std::ops::Deref;
 use std::slice;
+pub use std_hm::Keys;
+
+type Erased = (TypeId, Box<[u8]>);
 
 /// The polymorphic data store
 ///
@@ -18,7 +22,7 @@ use std::slice;
 #[derive(Debug)]
 // TODO: faster hash
 // FIXME: we cannot Drop values!
-pub struct Store<K>(HashMap<K, (TypeId, Box<[u8]>)>);
+pub struct Store<K>(HashMap<K, Erased>);
 
 impl<K> Default for Store<K> {
     fn default() -> Self {
@@ -48,9 +52,14 @@ impl<K> Store<K> {
     }
 
     // TODO: other ctors: with_capacity, with_hasher, ..., capacity, reserve, shrink_to_fit?
-    // TODO: iterators: key tags
     // TODO: access to the hasher?
-    // TODO: entry API
+
+    /// Returns an iterator over all keys in arbitrary order
+    ///
+    /// These are *not* tagged keys; only the bare key is stored.
+    pub fn keys(&self) -> Keys<K, (TypeId, Box<[u8]>)> {
+        self.0.keys()
+    }
 
     /// Returns the number of elements in the store
     pub fn len(&self) -> usize {
@@ -72,6 +81,11 @@ impl<K: Eq + Hash> Store<K> {
     /// Checks whether a value matching the specified `key` exists
     pub fn contains_key(&self, key: &K) -> bool {
         self.0.contains_key(key)
+    }
+
+    /// Get the [`TypeId`] of a stored value
+    pub fn get_type_id(&self, key: &K) -> Option<TypeId> {
+        self.0.get(key).map(|v| v.0)
     }
 
     /// Returns a reference to the value corresponding to the key
@@ -139,6 +153,165 @@ impl<K: Eq + Hash> Store<K> {
         self.0.insert(key.clone(), (TypeId::of::<V>(), type_erased));
         TaggedKey::new(key)
     }
+
+    /// Removes and returns a value, if present
+    ///
+    /// Panics on type mismatch.
+    pub fn remove_boxed<V: 'static>(&mut self, key: &TaggedKey<K, V>) -> Option<Box<V>> {
+        self.0.remove(key).map(|v| {
+            assert_eq!(
+                v.0,
+                TypeId::of::<V>(),
+                "Store::remove_boxed: type V={} does not match type id",
+                type_name::<V>()
+            );
+            unsafe {
+                let slice: &mut [u8] = &mut *Box::into_raw(v.1);
+                debug_assert_eq!(slice.len(), size_of::<V>());
+                Box::from_raw(slice.as_mut_ptr() as *mut V)
+            }
+        })
+    }
+
+    /// Get `key`'s entry for in-place manipulation
+    pub fn entry<V: 'static>(&mut self, key: K) -> Entry<K, V> {
+        match self.0.entry(key) {
+            std_hm::Entry::Occupied(entry) => {
+                Entry::Occupied(OccupiedEntry(entry, Default::default()))
+            }
+            std_hm::Entry::Vacant(entry) => Entry::Vacant(VacantEntry(entry, Default::default())),
+        }
+    }
+}
+
+/// A view into a single entry in a map
+pub enum Entry<'a, K: 'a, V: 'static> {
+    Occupied(OccupiedEntry<'a, K, V>),
+    Vacant(VacantEntry<'a, K, V>),
+}
+
+impl<'a, K: 'a, V: 'static> Entry<'a, K, V> {
+    /// Ensure a value exists and return its reference
+    pub fn or_insert(self, default: V) -> &'a mut V {
+        match self {
+            Entry::Occupied(entry) => entry.into_mut(),
+            Entry::Vacant(entry) => entry.insert(default),
+        }
+    }
+
+    /// Ensure a value exists and return its reference
+    pub fn or_insert_with<F: FnOnce() -> V>(self, default: F) -> &'a mut V {
+        match self {
+            Entry::Occupied(entry) => entry.into_mut(),
+            Entry::Vacant(entry) => entry.insert(default()),
+        }
+    }
+}
+
+/// An occupied entry
+pub struct OccupiedEntry<'a, K: 'a, V: 'static>(
+    std_hm::OccupiedEntry<'a, K, Erased>,
+    PhantomData<V>,
+);
+impl<'a, K: 'a, V: 'static> OccupiedEntry<'a, K, V> {
+    /// Get the entry
+    ///
+    /// Panics on type mismatch.
+    pub fn get(&self) -> &V {
+        let v = self.0.get();
+        assert_eq!(
+            v.0,
+            TypeId::of::<V>(),
+            "OccupiedEntry::get: type V={} does not match type id",
+            type_name::<V>()
+        );
+        let slice: &[u8] = &*v.1;
+        debug_assert_eq!(slice.len(), size_of::<V>());
+        let p = slice.as_ptr() as *const u8 as *const V;
+        unsafe { &*p }
+    }
+
+    /// Get the entry, mutably
+    ///
+    /// Panics on type mismatch.
+    pub fn get_mut(&mut self) -> &mut V {
+        let v = self.0.get_mut();
+        assert_eq!(
+            v.0,
+            TypeId::of::<V>(),
+            "OccupiedEntry::get_mut: type V={} does not match type id",
+            type_name::<V>()
+        );
+        let slice: &mut [u8] = &mut *v.1;
+        debug_assert_eq!(slice.len(), size_of::<V>());
+        let p = slice.as_ptr() as *mut u8 as *mut V;
+        unsafe { &mut *p }
+    }
+
+    /// Convert into a mutable reference
+    ///
+    /// Panics on type mismatch.
+    pub fn into_mut(self) -> &'a mut V {
+        let v = self.0.into_mut();
+        assert_eq!(
+            v.0,
+            TypeId::of::<V>(),
+            "OccupiedEntry::into_mut: type V={} does not match type id",
+            type_name::<V>()
+        );
+        let slice: &mut [u8] = &mut *v.1;
+        debug_assert_eq!(slice.len(), size_of::<V>());
+        let p = slice.as_ptr() as *mut u8 as *mut V;
+        unsafe { &mut *p }
+    }
+
+    /// Sets the value of the entry
+    pub fn insert(&mut self, value: V) {
+        let boxed = Box::new(value);
+        let type_erased = unsafe {
+            let raw = Box::into_raw(boxed) as *mut u8;
+            let slice = slice::from_raw_parts_mut(raw, size_of::<V>());
+            Box::<[u8]>::from_raw(slice)
+        };
+        self.0.insert((TypeId::of::<V>(), type_erased));
+    }
+
+    /// Removes and returns the entry's value
+    ///
+    /// Panics on type mismatch.
+    pub fn remove_boxed(self) -> Box<V> {
+        let v = self.0.remove();
+        assert_eq!(
+            v.0,
+            TypeId::of::<V>(),
+            "OccupiedEntry::remove_boxed: type V={} does not match type id",
+            type_name::<V>()
+        );
+        unsafe {
+            let slice: &mut [u8] = &mut *Box::into_raw(v.1);
+            debug_assert_eq!(slice.len(), size_of::<V>());
+            Box::from_raw(slice.as_mut_ptr() as *mut V)
+        }
+    }
+}
+
+/// A vacant entry
+pub struct VacantEntry<'a, K: 'a, V: 'static>(std_hm::VacantEntry<'a, K, Erased>, PhantomData<V>);
+impl<'a, K: 'a, V: 'static> VacantEntry<'a, K, V> {
+    /// Sets the value of the entry and returns a mutable reference to it
+    pub fn insert(self, value: V) -> &'a mut V {
+        let boxed = Box::new(value);
+        let type_erased = unsafe {
+            let raw = Box::into_raw(boxed) as *mut u8;
+            let slice = slice::from_raw_parts_mut(raw, size_of::<V>());
+            Box::<[u8]>::from_raw(slice)
+        };
+        let v = self.0.insert((TypeId::of::<V>(), type_erased));
+        let slice: &mut [u8] = &mut *v.1;
+        debug_assert_eq!(slice.len(), size_of::<V>());
+        let p = slice.as_ptr() as *mut u8 as *mut V;
+        unsafe { &mut *p }
+    }
 }
 
 #[cfg(test)]
@@ -168,5 +341,16 @@ mod tests {
         let mut store = Store::new();
         let k = store.insert(1, ());
         assert_eq!(store.get(&k), Some(&()));
+    }
+
+    #[test]
+    fn entry_api() {
+        let mut store = Store::new();
+        let k = 1;
+        *store.entry(k).or_insert(0) = 10;
+        assert_eq!(store.get_typed(&k), Some(&10));
+
+        *store.entry(k).or_insert(0) = 20;
+        assert_eq!(store.get_typed(&k), Some(&20));
     }
 }
